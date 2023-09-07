7f16d344929bd83411c9ee0fc20247b47faca057
MalikNoumanArshad <maliknoumanarshad@gmail.com>
2023-09-07 19:41:29 +0500

    Your new

--- src/App.js
+++ src/App.js
@@ -1,137 +1,195 @@
-import React,{useState} from 'react'
-import Header from './Header/Header'
-import Todo from './components/Todo'
+import React, { useState } from 'react';
 import Paper from '@mui/material/Paper';
+import {
+  Button,IconButton,Table,TableBody,TableCell,TableContainer, TableHead,TableRow,TextField,} from '@mui/material';
+import { Delete } from '@mui/icons-material';
 
-import { Button, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TextField } from '@mui/material'
 const App = () => {
-  const [task,settask] = useState("");
-  const [startdate,setstartdate] = useState("");
-  const [enddate,setenddate] = useState("");
+  const [task, setTask] = useState('');
+  const [startdate, setStartDate] = useState('');
+  const [enddate, setEndDate] = useState('');
+  const [selectedTaskIndex, setSelectedTaskIndex] = useState(null);
 
   const [todo, setTodo] = useState([]);
-  function deleteTodo(index) {
-    const updatedTodo = todo.filter((_, i) => i !== index);
-    setTodo(updatedTodo);
-  }
-  function todocall() {
-    if (task !== '' && startdate !== '' && enddate !== '') {
-      const newStartDate = new Date(startdate);
-      const newEndDate = new Date(enddate);
-  
-      const hasConflict = todo.some((item) => {
-        const itemStartDate = new Date(item.Liststartdate);
-        const itemEndDate = new Date(item.Listenddate);
-  
-        return (
-          (newStartDate >= itemStartDate && newStartDate <= itemEndDate) ||
-          (newEndDate >= itemStartDate && newEndDate <= itemEndDate) ||
-          (newStartDate <= itemStartDate && newEndDate >= itemEndDate)
-        );
-      });
-  
-      if (hasConflict) {
-        alert("Task conflicts with an existing task. Please choose different dates.");
-      } else {
-        let listitem = {
-          Listtask: task,
-          Liststartdate: startdate,
-          Listenddate: enddate,
-        };
-        let updatetodo = [...todo, listitem];
-        setTodo(updatetodo);
-        settask("");
-        setenddate("");
-        setstartdate("");
-      }
-    } else {
-      alert("Fields cannot be empty");
+
+  const isEditMode = selectedTaskIndex !== null;
+
+  const handleFormSubmit = () => {
+    if (task.trim() === '' || startdate === '' || enddate === '') {
+      alert('Fields cannot be empty');
+      return;
     }
-  }
-  
-  
-  
 
+    const newStartDate = new Date(startdate);
+    const newEndDate = new Date(enddate);
 
-return (
-  <>
-<center>  
-  <div style={{padding:'30px',}}>
+    const hasConflict = todo.some((item, index) => {
+      if (index === selectedTaskIndex) return false;
 
-  <p style={{fontSize:'26px'}}>Todo List</p>
+      const itemStartDate = new Date(item.Liststartdate);
+      const itemEndDate = new Date(item.Listenddate);
 
+      return (
+        (newStartDate >= itemStartDate && newStartDate <= itemEndDate) ||
+        (newEndDate >= itemStartDate && newEndDate <= itemEndDate) ||
+        (newStartDate <= itemStartDate && newEndDate >= itemEndDate)
+      );
+    });
 
+    if (hasConflict) {
+      alert('Event already added. Please choose different dates.');
+      return;
+    }
 
-      
-      <form style={{width:'300px'}}>
-      <label>Task Name</label>
-      <br />
-      <TextField fullWidth type='text' value={task} onChange={(e)=> settask(e.target.value)} required />
-      <br />
-      <br />
-      <label>Start Date</label>
-      <br />
-      <TextField fullWidth type='date' value={startdate} onChange={(e)=> setstartdate(e.target.value)} required />
-      <br />
-      <br />
-      <label>End Date</label>
-      <br />
-      <TextField fullWidth type='date' value={enddate} onChange={(e)=> setenddate(e.target.value)} required />
-      <br />
-      <br />
+    if (isEditMode) {
+      const updatedTodo = [...todo];
+      updatedTodo[selectedTaskIndex] = {
+        ...updatedTodo[selectedTaskIndex],
+        Listtask: task,
+        Liststartdate: startdate,
+        Listenddate: enddate,
+      };
+      setTodo(updatedTodo);
+      clearForm();
+    } else {
+      const listitem = {
+        Listtask: task,
+        Liststartdate: startdate,
+        Listenddate: enddate,
+      };
+      const updatedTodo = [...todo, listitem];
+      setTodo(updatedTodo);
+      clearForm();
+    }
+  };
 
-      <Button fullWidth type='submit' onClick={todocall} >Submit</Button>
-      <br />
-  <br />
-  </form>
+  const clearForm = () => {
+    setTask('');
+    setStartDate('');
+    setEndDate('');
+    setSelectedTaskIndex(null);
+  };
 
-  <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell align="right">Task Name</TableCell>
-            <TableCell align="right">Start Date</TableCell>
-            <TableCell align="right">End Date</TableCell>
-            <TableCell align="right">Status</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {todo.map((item,index) =>{
-          const currentDate = new Date();
-          const endDate = new Date(item.Listenddate);
-          let status = "Due";
-          if (currentDate > endDate) {
-            status = "Overdue";
-          } else if (currentDate.toDateString() === endDate.toDateString()) {
-            status = "Due Today";
-          }
-          return(
+  const editTask = (index) => {
+    const selectedTask = todo[index];
+    setTask(selectedTask.Listtask);
+    setStartDate(selectedTask.Liststartdate);
+    setEndDate(selectedTask.Listenddate);
+    setSelectedTaskIndex(index);
+  };
 
-            <TableRow
-              key={index}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              
-              <TableCell align="right">{index+1}</TableCell>
-              <TableCell align="right">{item.Listtask}</TableCell>
-              <TableCell align="right">{item.Liststartdate}</TableCell>
-              <TableCell align="right">{item.Listenddate}</TableCell>
-              <TableCell align="right">{status}</TableCell>
-              <TableCell align="right">  
-                    <button onClick={() => deleteTodo(index)}>Delete</button>
-</TableCell>
-            </TableRow>
-          )})}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  
-  </div>
-  </center>
-  </>
-)
-}
+  const deleteTask = (index) => {
+    const updatedTodo = [...todo];
+    updatedTodo.splice(index, 1);
+    setTodo(updatedTodo);
+  };
 
-export default App
+  return (
+    <center>
+      <div style={{ padding: '30px' }}>
+        <p style={{ fontSize: '26px' }}>Todo List</p>
+        <form style={{ width: '300px' }}>
+          <label>Event Name</label>
+          <br />
+          <TextField
+            fullWidth
+            type="text"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+          <label>Start Date</label>
+          <br />
+          <TextField
+            fullWidth
+            type="datetime-local"
+            value={startdate}
+            onChange={(e) => setStartDate(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+          <label>End Date</label>
+          <br />
+          <TextField
+            fullWidth
+            type="datetime-local"
+            value={enddate}
+            onChange={(e) => setEndDate(e.target.value)}
+            required
+          />
+          <br />
+          <br />
+
+          <Button variant="contained" fullWidth type="button" onClick={handleFormSubmit}>
+            {isEditMode ? 'Update' : 'Submit'}
+          </Button>
+          <br />
+          <br />
+        </form>
+
+        <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>ID</TableCell>
+                <TableCell align="right">Event Name</TableCell>
+                <TableCell align="right">Start Date</TableCell>
+                <TableCell align="right">Start Time</TableCell>
+                <TableCell align="right">End Date</TableCell>
+                <TableCell align="right">End Time</TableCell>
+                <TableCell align="right">Status</TableCell>
+                <TableCell align="right">Actions</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {todo.map((item, index) => {
+                const currentDate = new Date();
+                const endDate = new Date(item.Listenddate);
+                let status = 'Due';
+                if (currentDate > endDate) {
+                  status = 'Overdue';
+                } else if (currentDate.toDateString() === endDate.toDateString()) {
+                  status = 'Due Today';
+                }
+
+                const startDateObj = new Date(item.Liststartdate);
+                const startDateDate = startDateObj.toLocaleDateString();
+                const startTime = startDateObj.toLocaleTimeString();
+                const endDateObj = new Date(item.Listenddate);
+                const endDateDate = endDateObj.toLocaleDateString();
+                const endTime = endDateObj.toLocaleTimeString();
+
+                return (
+                  <TableRow
+                    key={index}
+                    sx={{
+                      '&:last-child td, &:last-child th': { border: 0 },
+                    }}
+                  >
+                    <TableCell align="right">{index + 1}</TableCell>
+                    <TableCell align="right">{item.Listtask}</TableCell>
+                    <TableCell align="right">{startDateDate}</TableCell>
+                    <TableCell align="right">{startTime}</TableCell>
+                    <TableCell align="right">{endDateDate}</TableCell>
+                    <TableCell align="right">{endTime}</TableCell>
+                    <TableCell align="right">{status}</TableCell>
+                    <TableCell align="right">
+                 
+                      <Button onClick={() => editTask(index)}>Edit</Button>
+                      <Button onClick={() => deleteTask(index)}>Delete</Button>
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </center>
+  );
+};
+
+export default App;
